52d832497670ea8b654d7875cf67efd6f3b11c43
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-05-23 16:16:43 +0100

    add the two singles finding the sum

--- src/test/scala/SumOfSinglesSpec.scala
+++ src/test/scala/SumOfSinglesSpec.scala
@@ -7,6 +7,8 @@ class SumOfSinglesSpec  extends WordSpec with MustMatchers {
     "when given a single input return the input as an output" in {
       SumOfSingles.Calculator(List(1)) mustEqual 1
     }
-
+    "when given a two unique input return the sum of the inputs" in {
+      SumOfSingles.Calculator(List(1, 2)) mustEqual 3
+    }
   }
 }
