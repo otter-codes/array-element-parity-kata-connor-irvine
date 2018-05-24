05bd0a08c0d60449eac0808eeccfe425aa195f93
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-05-24 08:56:53 +0100

    add this has both +/- entrys and still passes for combining two unique ans

--- src/test/scala/SumOfSinglesSpec.scala
+++ src/test/scala/SumOfSinglesSpec.scala
@@ -19,5 +19,11 @@ class SumOfSinglesSpec  extends WordSpec with MustMatchers {
     "when given an input return with a three pairs (+/-) eliminate them and leave the unique element" in {
       SumOfSingles.Calculator(List(-1, -1, 2, 2, 3, -4, -4)) mustEqual 3
     }
+    "when given an input return with a three pairs (+) eliminate them and two unique elements, return the sum" in {
+      SumOfSingles.Calculator(List(1, 1, 2, 2, 3, 4, 4, 5)) mustEqual 8
+    }
+    "when given an input return with a three pairs (+/-) eliminate them and two unique elements, return the sum" in {
+      SumOfSingles.Calculator(List(-1, -1, 2, 2, -3, -3, 4, 5)) mustEqual 9
+    }
   }
 }
